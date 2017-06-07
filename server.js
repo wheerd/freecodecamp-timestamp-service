3ee000e469bebc91b952116ae20d807712df3053
Manuel Krebber <admin@wheerd.de>
2017-06-07 15:01:06 +0000

    Implemented the service.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const moment = require('moment')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:date', function (req, res) {
+    var parsed = null;
+    if (req.params.date.match(/^\d+$/)) {
+        parsed = moment(parseInt(req.params.date) * 1000);
+    } else {
+        parsed = moment(req.params.date);
+    }
+    if (!parsed.isValid()) {
+        res.json({ unix: null, natural: null });
+    } else {
+        res.json({
+            unix: parseInt(parsed.format('X')),
+            natural: parsed.format('MMMM D, YYYY')
+        });
+    }
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Timestamp service listening on port 8080!')
 })
